8cd518d9dd5634ec4e0f32fbe43db2e23ee99a70
Mary <mari.front.74@gmail.com>
2025-04-10 09:17:13 +0500

    feat: add new task

---
+++ 8kuy/Training JS 3 Basic data types String.js
@@ -0,0 +1,45 @@
+/**
+ * В javascript String является одним из основных типов данных.
+ * Чтобы объявить строковую переменную, используйте "" или ''.
+ * Это могут быть буквы:a,b,c,A,B,C, цифры: 123..., различные символы:@#$%^&* , управляющие символы:\n,\t и т.д.
+ *
+ * Строка может использовать операторы +, соединять две или более строк вместе.
+ *
+ * Задание 1: Я создал три функции и определил несколько глобальных переменных, пожалуйста, выберите несколько переменных, которые могут составить имя функции, и верните их (обратите внимание, что заглавные и строчные буквы отличаются).
+ *
+ * Когда вы закончите работу, нажмите кнопку "Run Tests",
+ * чтобы увидеть, если ваш код работает правильно.
+ *
+ * misson 2: После misson 1 закончил. вы должны нажать кнопку "Attempt"
+ * чтобы увидеть мои три вопроса, и написать ответ в функции answer1, answer2, answer3
+ *
+ * Если все правильно, нажмите кнопку "Submit" снова, чтобы представить ваш код пройти это ката.
+ */
+
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+function Dad(){
+    //select some variable to combine "Dad"
+    return d1+a2+d2;
+}
+function Bee(){
+    //select some variable to combine "Bee"
+    return b1+e2+e2;
+}
+function banana(){
+    //select some variable to combine "banana"
+    return b2+a2+n2+a2+n2+a2;
+}
+
+//answer some questions if you finished works above
+function answer1(){
+    //the answer should be "yes" or "no"
+    return "no";
+}
+function answer2(){
+    //the answer should be "yes" or "no"
+    return "no";
+}
+function answer3(){
+    //the answer should be "yes" or "no"
+    return "yes";
+}
