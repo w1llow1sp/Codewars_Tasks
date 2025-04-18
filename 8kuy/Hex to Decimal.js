18cc61220db38214f8521f64b06eae32f6bf8def
Mary <mari.front.74@gmail.com>
2025-04-18 10:22:55 +0500

    feat:add new solved tasks

---
+++ 8kuy/Hex to Decimal.js
@@ -0,0 +1,14 @@
+/**
+ * Выполните функцию, которая преобразует
+ * шестнадцатеричное число (заданное в виде строки) в десятичное.
+ */
+
+function hexToDec(hexString){
+    return (parseInt(hexString,16))
+}
+
+hexToDec("1")
+hexToDec("a")
+hexToDec("10")
+hexToDec("FF")
+hexToDec("FF")
